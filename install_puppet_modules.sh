bda72d9937ba19ac0ed63c85b4fba6923f53cda0
root <root@ip-172-31-8-170.us-west-1.compute.internal>
2018-04-20 06:47:39 +0000

    adding script to install base puppet modules

---
+++ install_puppet_modules.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#puppet labs developed modules
+/opt/puppetlabs/bin/puppet module install puppetlabs-windows --modulepath /etc/puppetlabs/code/modules
+/opt/puppetlabs/bin/puppet module install puppetlabs-stdlib --modulepath /etc/puppetlabs/code/modules
+/opt/puppetlabs/bin/puppet module install puppetlabs-ntp --modulepath /etc/puppetlabs/code/modules
+/opt/puppetlabs/bin/puppet module install puppetlabs-apache --modulepath /etc/puppetlabs/code/modules
+/opt/puppetlabs/bin/puppet module install puppetlabs-puppetdb --modulepath /etc/puppetlabs/code/modules
+/opt/puppetlabs/bin/puppet module install puppetlabs-mysql --modulepath /etc/puppetlabs/code/modules
+/opt/puppetlabs/bin/puppet module install puppetlabs-vcsrepo --modulepath /etc/puppetlabs/code/modules
+
+#puppet forge approved modules
+/opt/puppetlabs/bin/puppet module install stankevich-python --modulepath /opt/puppetlabs/puppet/modules
+/opt/puppetlabs/bin/puppet module install maestrodev-wget --modulepath /opt/puppetlabs/puppet/modules
